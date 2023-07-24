7207c0073fe52dbf7a134843974bf44c8cdacd95
George Sakellaropoulos <sakellaropoulos.giorgos@hotmail.com>
2023-07-24 12:40:22 +0300

    dev loc

--- screens/SalesTabsScreen.js
+++ screens/SalesTabsScreen.js
@@ -4,12 +4,15 @@ import SalesStatisticsScreen from './SalesStatisticsScreen';
 import SalesInsightsScreen from './SalesInsightsScreen';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { useTranslation } from 'react-i18next';
+import i18next from '../languages/i18n';
 
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Tab = createBottomTabNavigator();
 
 const SalesTabsScreen = () => {
+  const { t, i18n } = useTranslation();
   return (
     <Tab.Navigator
       screenOptions={{
@@ -18,16 +21,17 @@ const SalesTabsScreen = () => {
         tabBarStyle: {
           backgroundColor: 'rgb(37, 99, 235)',
         },
-        headerStyle: {backgroundColor: 'rgb(37, 99, 235)'},
-        headerTitleStyle: {fontSize: 25, color: 'white'},
+        headerStyle: { backgroundColor: 'rgb(37, 99, 235)' },
+        headerTitleStyle: { fontSize: 25, color: 'white' },
         headerTitleAlign: 'center',
       }}>
       <Tab.Screen
-        name={'Sales statistics'}
+        // name={'Sales statistics'}
+        name={t("SalesStatistics")}
         component={SalesStatisticsScreen}
         options={{
           headerShown: false,
-          tabBarIcon: ({focused}) => (
+          tabBarIcon: ({ focused }) => (
             <Ionicons
               name="md-stats-chart"
               size={25}
@@ -37,11 +41,12 @@ const SalesTabsScreen = () => {
         }}
       />
       <Tab.Screen
-        name={'Sales insights'}
+        // name={'Sales insights'}
+        name={t("SalesInsights")}
         component={SalesInsightsScreen}
         options={{
           headerShown: false,
-          tabBarIcon: ({focused}) => (
+          tabBarIcon: ({ focused }) => (
             <MaterialIcons
               name="insights"
               size={25}
