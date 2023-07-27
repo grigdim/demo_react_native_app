c5508589100d8d4987f39a57a346a3eaff4b73bc
Dim Grigoriadis <grigdim@Dims-MacBook-Pro.local>
2023-07-27 14:49:48 +0300

    insights seasonality details

--- screens/SalesInsightsSeasonalityScreen.js
+++ screens/SalesInsightsSeasonalityScreen.js
@@ -1,3 +1,4 @@
+/* eslint-disable dot-notation */
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable react-hooks/exhaustive-deps */
 import {
@@ -22,6 +23,8 @@ const SalesInsightsSeasonalityScreen = () => {
   const [loading, setLoading] = useState(false);
   const [categories, setCategories] = useState([]);
   const [seasonalityPerCategory, setSeasonalityPerCategory] = useState([]);
+  const [seasonalityDetailsPerCategory, setSeasonalityDetailsPerCategory] =
+    useState([]);
 
   const getBackgroundColor = value => {
     if (value === 0) {
@@ -52,11 +55,13 @@ const SalesInsightsSeasonalityScreen = () => {
         requestOptions,
       );
       const data = await response.json();
-      console.log(data);
       setCategories(data);
       let seasonalityPerCategoryArr = [];
       const promises = [];
-
+      const collator = new Intl.Collator(undefined, {
+        sensitivity: 'base',
+        ignorePunctuation: true,
+      });
       data.map(item => {
         const url = `http://${ip}:3000/bo/Reports/GetSeasonality?productCategoryName=${item}`;
         promises.push(async () => {
@@ -70,7 +75,6 @@ const SalesInsightsSeasonalityScreen = () => {
       let seasonalityPerCategoryArrFormatted = [];
       seasonalityPerCategoryArr.map(item => {
         let obj = {};
-        console.log(item);
         obj['Category'] = item[0].Category;
         obj['currentMonth'] = {
           DateMonth: item[0].DateMonth,
@@ -84,7 +88,73 @@ const SalesInsightsSeasonalityScreen = () => {
         };
         seasonalityPerCategoryArrFormatted.push(obj);
       });
-      setSeasonalityPerCategory(seasonalityPerCategoryArrFormatted);
+      setSeasonalityPerCategory(
+        seasonalityPerCategoryArrFormatted.sort((a, b) => {
+          const categoryA = a.Category;
+          const categoryB = b.Category;
+          return collator.compare(categoryA, categoryB);
+        }),
+      );
+
+      let seasonalityDetailsPerCategoryArr = [];
+      const promises2 = [];
+      data.map(item => {
+        const url = `http://${ip}:3000/bo/Reports/GetSeasonalityDetails?productCategoryName=${item}`;
+        promises2.push(async () => {
+          const response2 = await fetch(url, requestOptions);
+          const data2 = await response2.json();
+          seasonalityDetailsPerCategoryArr.push(data2);
+        });
+      });
+
+      await Promise.all(promises2.map(promise => promise()));
+      let seasonalityDetailsPerCategoryArrFormatted =
+        seasonalityDetailsPerCategoryArr.map(arr => {
+          const obj = {
+            Category: arr[0].Category,
+            SeasonalityDetailsPerSubCategory: [],
+          };
+
+          arr.forEach(item => {
+            const existingSubcategory = obj[
+              'SeasonalityDetailsPerSubCategory'
+            ].find(subCatItem => subCatItem.Subcategory === item.Subcategory);
+
+            if (!existingSubcategory) {
+              const newSubcategory = {
+                Subcategory: item.Subcategory,
+                Details: [
+                  {
+                    currentMonth: item.DateMonth,
+                    MonthName: item.MonthName,
+                    Seasonality: item.Seasonality,
+                  },
+                ],
+              };
+              obj['SeasonalityDetailsPerSubCategory'].push(newSubcategory);
+            } else {
+              existingSubcategory.Details.push({
+                nextMonth: item.DateMonth,
+                MonthName: item.MonthName,
+                Seasonality: item.Seasonality,
+              });
+            }
+          });
+
+          return obj;
+        });
+
+      setSeasonalityDetailsPerCategory(
+        seasonalityDetailsPerCategoryArrFormatted.sort((a, b) => {
+          const categoryA = a.Category;
+          const categoryB = b.Category;
+          return collator.compare(categoryA, categoryB);
+        }),
+      );
+
+      console.log('====================================');
+      console.log(seasonalityDetailsPerCategoryArrFormatted);
+      console.log('====================================');
       // end of request
       // setLoading(false);
     }
@@ -101,21 +171,21 @@ const SalesInsightsSeasonalityScreen = () => {
       {loading ? (
         <ActivityIndicator color="rgb(34 211 238)" size="large" />
       ) : (
-        <ScrollView className="w-full space-y-4">
+        <ScrollView className="space-y-4">
           {/*Seasonality per product category start*/}
           {seasonalityPerCategory.length > 0 && (
-            <ScrollView horizontal>
+            <ScrollView horizontal className="">
               <View
                 className="bg-white m-4 p-4 space-y-2 rounded-lg"
                 style={{elevation: 10}}>
                 {seasonalityPerCategory.map(item => (
-                  <View className="flex-row space-x-4 p-1 rounded-md">
-                    <Text className="grow-1">{item.Category}</Text>
-                    <Text className="bg-slate-600 rounded-md px-2 text-white">
+                  <View className="flex-row space-x-4 p-1 mr-0 rounded-md">
+                    <Text className="">{item.Category}</Text>
+                    <Text className="bg-slate-600 rounded-md px-2 text-white ">
                       {item.currentMonth.MonthName}
                     </Text>
                     <Text
-                      className="px-2 rounded-md"
+                      className="px-2 rounded-md "
                       style={{
                         backgroundColor: getBackgroundColor(
                           item.currentMonth.Seasonality,
@@ -123,11 +193,11 @@ const SalesInsightsSeasonalityScreen = () => {
                       }}>
                       {item.currentMonth.Seasonality}%
                     </Text>
-                    <Text className="bg-slate-600 rounded-md px-2 text-white">
+                    <Text className="bg-slate-600 rounded-md px-2 text-white ">
                       {item.nextMonth.MonthName}
                     </Text>
                     <Text
-                      className="px-2 rounded-md"
+                      className="px-2 rounded-md "
                       style={{
                         backgroundColor: getBackgroundColor(
                           item.nextMonth.Seasonality,
@@ -143,7 +213,56 @@ const SalesInsightsSeasonalityScreen = () => {
           {/*Seasonality per product category end*/}
 
           {/*Seasonality per product category with sub categories start*/}
-          <View></View>
+          {seasonalityDetailsPerCategory.length > 0 && (
+            <ScrollView horizontal>
+              <View
+                className="bg-white m-4 p-4 space-y-2 rounded-lg divide-y-2 divide-gray-500"
+                style={{elevation: 10}}>
+                {seasonalityDetailsPerCategory.map(item => (
+                  <View className="space-x-4 p-1 flex-row">
+                    <Text className="w-1/4 text-left font-bold">
+                      {item.Category}
+                    </Text>
+                    <View className="space-y-2 divide-y divide-gray-300">
+                      {item.SeasonalityDetailsPerSubCategory.map(obj => {
+                        return (
+                          <View className="flex-row space-x-2 py-1 justify-between items-center">
+                            <Text className="">{obj.Subcategory}</Text>
+                            <View className="flex-row space-x-2">
+                              <Text className="bg-slate-600 text-white px-2 rounded-md text-left">
+                                {obj.Details[0].MonthName}
+                              </Text>
+                              <Text
+                                style={{
+                                  backgroundColor: getBackgroundColor(
+                                    obj.Details[0].Seasonality,
+                                  ),
+                                }}
+                                className="rounded-md px-2">
+                                {obj.Details[0].Seasonality}%
+                              </Text>
+                              <Text className="bg-slate-600 text-white px-2 rounded-md">
+                                {obj.Details[1].MonthName}
+                              </Text>
+                              <Text
+                                style={{
+                                  backgroundColor: getBackgroundColor(
+                                    obj.Details[1].Seasonality,
+                                  ),
+                                }}
+                                className="rounded-md px-2">
+                                {obj.Details[1].Seasonality}%
+                              </Text>
+                            </View>
+                          </View>
+                        );
+                      })}
+                    </View>
+                  </View>
+                ))}
+              </View>
+            </ScrollView>
+          )}
           {/*Seasonality per product category with sub categories end*/}
         </ScrollView>
       )}
